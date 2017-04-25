6a4376b6884e7f880cc33b65ceef0281ad9e8beb
Aaron <963076844@qq.com>
2017-04-26 01:04:09 +0800

    9090

--- app/models.py
+++ app/models.py
@@ -8,10 +8,22 @@ class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key = True)
   email = db.Column(db.String(64), unique=True, index=True)
   username = db.Column(db.String(64), unique=True, index=True)
-  #password_hash = db.Column(db.String(128))
+  password_hash = db.Column(db.String(128))
   password = db.Column(db.String(128))
   role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
   # ...
+
+  @login_manager.user_loader
+  def load_user(user_id):
+    return User.query.get(int(user_id))
+  @property
+  def password(self):
+    raise AttributeError('password is not a readable attribute')
+  @password.setter
+  def password(self, password):
+    self.password_hash = generate_password_hash(password)
+  def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
 
 class Role(db.Model):
   __tablename__ = 'roles'
@@ -20,16 +32,3 @@ class Role(db.Model):
   default = db.Column(db.Boolean, default=False, index=True)
   permissions = db.Column(db.Integer)
   users = db.relationship('User', backref='role', lazy='dynamic')
-@login_manager.user_loader
-def load_user(user_id):
-  return User.query.get(int(user_id))
-
-password_hash = db.Column(db.String(128))
-@property
-def password(self):
-  raise AttributeError('password is not a readable attribute')
-@password.setter
-def password(self, password):
-  self.password_hash = generate_password_hash(password)
-def verify_password(self, password):
-  return check_password_hash(self.password_hash, password)
