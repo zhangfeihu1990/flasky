25a03516002d48c1bc0e292925b30722f999c925
sino_zhang <963076844@qq.com>
2017-06-01 18:13:30 +0800

    配置文件修改每页显示数量@CBD国际大厦

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 3
-    FLASKY_FOLLOWERS_PER_PAGE = 50
-    FLASKY_COMMENTS_PER_PAGE = 30
+    FLASKY_FOLLOWERS_PER_PAGE = 3
+    FLASKY_COMMENTS_PER_PAGE = 3
     @staticmethod
     def init_app(app):
         pass
